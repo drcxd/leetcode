54720f81fb6b48f8d2549642570df7ec80136c7f
Xiaoduan <drcxd@sina.com>
2019-11-19 21:15:53 -0500

    adapt a new faster solution of problem 152

--- src/152.maximum-product-subarray.cpp
+++ src/152.maximum-product-subarray.cpp
@@ -38,84 +38,25 @@ using namespace std;
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        if (nums.size() == 0) {
-            return 0;
-        }
-        if (nums.size() == 1) {
-            return nums[0];
-        }
-        vector<vector<int>> intervals;
-        vector<int> v;
-        bool has_zero = false;
+        int res = INT_MIN;
+        int cur = 1;
         for (int i = 0; i < nums.size(); ++i) {
-            if (nums[i] == 0) {
-                has_zero = true;
-                if (!v.empty()) {
-                    intervals.push_back(v);
-                    v.clear();
-                }
-            } else {
-                v.push_back(nums[i]);
-            }
-        }
-        if (!v.empty()) {
-            intervals.push_back(v);
-        }
-        if (has_zero) {
-            return max(maxInIntervals(intervals), 0);
-        }
-        return maxInIntervals(intervals);
-    }
-
-    int maxInIntervals(vector<vector<int>> &intervals) {
-        vector<int> maxs;
-        for (auto &v : intervals) {
-
-            if (v.size() == 1) {
-                maxs.push_back(v[0]);
-                continue;
-            }
-
-            int left = -1, right = -1;
-            int count = 0;
-            for (int i = 0; i < v.size(); ++i) {
-                if (v[i] < 0) {
-                    if (left < 0) {
-                        left = i;
-                    }
-                    if (i > right) {
-                        right = i;
-                    }
-                    ++count;
-                }
-            }
-
-            if (count % 2 == 0) {
-                int r = 1;
-                for (int i : v) {
-                    r *= i;
-                }
-                maxs.push_back(r);
-            } else {
-                int r = 1, l = 1;
-                for (int i = left + 1; i < v.size(); ++i) {
-                    r *= v[i];
-                }
-
-                for (int i = 0; i < right; ++i) {
-                    l *= v[i];
-                }
-                maxs.push_back(max(r, l));
+            cur *= nums[i];
+            res = max(cur, res);
+            if (cur == 0) {
+                cur = 1;
             }
         }
 
-        int real_max = maxs[0];
-        for (int i = 1; i < maxs.size(); ++i) {
-            if (maxs[i] > real_max) {
-                real_max = maxs[i];
+        cur = 1;
+        for (int i = nums.size() - 1; i >= 0; --i) {
+            cur *= nums[i];
+            res = max(cur, res);
+            if (cur == 0) {
+                cur = 1;
             }
         }
-        return real_max;
+        return res;
     }
 };
 
