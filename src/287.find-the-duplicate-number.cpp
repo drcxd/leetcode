2723dcf025215242edbb00356b20d80e4395c184
xiaoduan <drcxd@sina.com>
2019-12-05 10:58:49 -0500

    add a new solution of problem 287

--- src/287.find-the-duplicate-number.cpp
+++ src/287.find-the-duplicate-number.cpp
@@ -1,18 +1,38 @@
 class Solution {
 public:
-    int findDuplicate(vector<int> &nums) {
-        int fast = 0, slow = 0;
-        do {
-            slow = nums[slow];
-            fast = nums[nums[fast]];
-        } while (fast != slow);
+    // int findDuplicate(vector<int> &nums) {
+    //     int fast = 0, slow = 0;
+    //     do {
+    //         slow = nums[slow];
+    //         fast = nums[nums[fast]];
+    //     } while (fast != slow);
 
-        fast = 0;
-        while (fast != slow) {
-            slow = nums[slow];
-            fast = nums[fast];
+    //     fast = 0;
+    //     while (fast != slow) {
+    //         slow = nums[slow];
+    //         fast = nums[fast];
+    //     }
+    //     return fast;
+    // }
+
+    int findDuplicate(vector<int> &nums) {
+        int low = 0, high = nums.size() - 1;
+        int mid;
+        while (low < high) {
+            mid = (low + high) / 2;
+            int count = 0;
+            for (int n : nums) {
+                if (n <= mid) {
+                    ++count;
+                }
+            }
+            if (count <= mid) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
         }
-        return fast;
+        return low;
     }
 private:
 };
